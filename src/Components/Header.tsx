f7909993770ba7f8f9b6f446929d1f4489f992ae
IFudor1a <alisher.berik222@gmail.com>
2022-09-27 23:25:54 +0600

    MainPage front-end

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import '../Styles/Header.scss';
+// @ts-ignore
+import Logo from '../Materials/Images/Logo full.png';
+import {BasketIconSVG, BottomArrowSVG, CameraIconSVG, OrderIconSVG, RegIconSVG, SearchIconSVG} from "../Materials/SVG";
+
 const TopHeader = () => {
     return (
         <div className='TopHeader'>
+            <div className='Logo'>
+                <img src={Logo} alt='SilkWay'/>
+            </div>
+            <div className='Search'>
+                <div className='Search_dropdown'>
+                    <div>Продукты</div>
+                    <div>{BottomArrowSVG}</div>
+                </div>
 
+                <div className='Search_Input'>
+                    <div className='Input_Holder'>
+                    {SearchIconSVG}
+                    <input placeholder='Что вы ищите'/>
+                    </div>
+                    {CameraIconSVG}
+                </div>
+            </div>
+            <div className='Futures'>
+                <div className='Futures_Item'>
+                    {OrderIconSVG}
+                    Заказы
+                </div>
+                <div className='Futures_Item'>
+                    {BasketIconSVG}
+                    Корзина
+                </div>
+                <div className='Futures_Item'>
+                    {RegIconSVG}
+                    <a href='#'>Вход</a> / <a href='#'>Регистрация</a>
+                </div>
+
+            </div>
         </div>
     )
 }
@@ -19,8 +54,8 @@ const BottomHeader = () => {
 const Header = () => {
     return (
         <div className='Header'>
-             <TopHeader/>
-             <BottomHeader/>
+            <TopHeader/>
+            <BottomHeader/>
         </div>
     );
 };
